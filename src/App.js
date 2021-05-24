abff76c771d97004c7cd83b844a50a46c2b7717e
flaviaursino <flavia.ursino@gmail.com>
2021-05-23 21:09:06 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import Titulo from "./components/titulo";
+import Texto from "./components/texto";
+
+export default function App() {
+  return (
+    <div>
+      <Titulo />
+      <Texto />
+    </div>
+  );
+}
